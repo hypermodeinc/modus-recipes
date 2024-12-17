4fd071665313828deaf5bc228144a6427d313242
Steph Dietz <steph@hypermode.com>
2024-12-16 20:34:32 -0600

    fetch movies based on page and search query

--- dgraph-modus-example/main.go
+++ dgraph-modus-example/main.go
@@ -5,44 +5,45 @@ import (
 	"github.com/hypermodeinc/modus/sdk/go/pkg/http"
 )
 
-// FetchMoviesAndActors queries the Dgraph endpoint for movies and actors
-func FetchMoviesAndActors() (string, error) {
-	// Define the query payload as a Go map
-	queryPayload := map[string]string{
-		"query": `
-		{
-			movies(func: has(initial_release_date), first: 10) {
-				uid
-				name@en
-				initial_release_date
-				starring {
-					name@en
-				}
-			}
-		}`,
-	}
+func FetchMoviesAndActorsWithPagination(page int, search string) (string, error) {
+	offset := page * 20
 
-	// Create the HTTP request options
+	query := fmt.Sprintf(`
+	{
+		movies(func: has(initial_release_date), first: 20, offset: %d) %s {
+			uid
+			name@en
+			initial_release_date
+			starring {
+				name@en
+			}
+		}
+	}`, offset, buildSearchFilter(search))
+
+	queryPayload := map[string]string{"query": query}
+
 	options := &http.RequestOptions{
 		Method: "POST",
-		Body:   queryPayload, // Directly pass the Go map as the body
+		Body:   queryPayload,
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
 	}
 
-	// Create the HTTP request
 	request := http.NewRequest("https://play.dgraph.io/query?respFormat=json", options)
-
-	// Send the request using Modus fetch
 	response, err := http.Fetch(request)
 	if err != nil {
 		fmt.Println("Error fetching data from Dgraph:", err)
 		return "", err
 	}
-	
-	fmt.Println("Response:", string(response.Body))
 
-	// Return the raw response body as a string
+	fmt.Println("Response:", string(response.Body))
 	return string(response.Body), nil
+}
+
+func buildSearchFilter(search string) string {
+	if search == "" {
+		return ""
+	}
+	return fmt.Sprintf(`@filter(anyofterms(name@en, "%s") OR anyofterms(starring.name@en, "%s"))`, search, search)
 }
